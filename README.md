b65565746ad871a7a6e9f660f5cfe47632551a44
Jonniek <kayhty@windowslive.com>
2020-11-30 21:56:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## Advent of code 2020
+
+My rust is still a bit rusty.
+
+Running:
+```
+cd dayXX/
+# create input file to input/input.txt
+cat input/input.txt | cargo run
+```
---
+++ day01/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{self, Read};
+
+fn main() {
+  let mut input = String::new();
+  io::stdin().read_to_string(&mut input).unwrap();
+
+  part1(&input);
+  part2(&input);
+}
+
+fn part1(input: &str) {
+}
+
+fn part2(input: &str) {
+}
