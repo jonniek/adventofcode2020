c0d829059bb8895d65869ee4376988f9ac132d68
Jonniek <kayhty@windowslive.com>
2020-12-01 17:21:08 +0200

    day01
    Simple bruteforce with minor optimizations

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,8 +8,29 @@ fn main() {
   part2(&input);
 }
 
+const LIMIT: i32 = 2020;
+
 fn part1(input: &str) {
+  let numbers: Vec<i32> = input.lines().map(|v| v.parse::<i32>().unwrap()).collect();
+  for (i, n) in numbers.iter().enumerate() {
+    for m in numbers.iter().skip(i) {
+      if n + m == LIMIT {
+        println!("{}", n * m)
+      }
+    }
+  }
 }
 
 fn part2(input: &str) {
+  let numbers: Vec<i32> = input.lines().map(|v| v.parse::<i32>().unwrap()).collect();
+  for (i, n) in numbers.iter().enumerate() {
+    for (j, m) in numbers.iter().enumerate().skip(i) {
+      if n + m >= LIMIT { continue }
+      for b in numbers.iter().skip(j) {
+        if n + m + b == LIMIT {
+          println!("{}", n * m * b)
+        }
+      }
+    }
+  }
 }
